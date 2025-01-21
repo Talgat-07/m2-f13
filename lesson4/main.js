6614492e0492aef555fafd5891a1464b28d6c1e8
Talgat-07 <talakv.07@gmail.com>
2025-01-21 12:51:01 +0600

    lesson 4

---
+++ lesson4/main.js
@@ -0,0 +1,51 @@
+// const arr = [1, 2, 43, 5, 34, 5];
+
+//map
+
+// const f = arr.findIndex((el) => el === 34);
+
+// console.log(f);
+
+// arr.pop
+// arr.shift
+// arr.push
+// arr.unshift
+
+// console.log(arr);
+
+// Метод pop() удаляет последний элемент из массива и возвращает его значение.
+// const a = arr.pop();
+
+// Метод shift() удаляет первый элемент из массива и возвращает его значение. Этот метод изменяет длину массива.
+// const a = arr.shift();
+
+// arr.push(100);
+// arr.unshift(99);
+
+// console.log(arr);
+
+//filter
+
+// console.log(arr);
+
+// const filter = arr.filter((el) => el > 10);
+
+// const total = arr.reduce((acc, curr) => acc + curr, 0);
+
+// const join = arr
+//   .join(" ")
+//   .split(" ")
+//   .map((el) => Number(`${el}0`));
+
+// arr.splice(3, 3);
+// arr.splice(2, 0, 100);
+// const boys = ["Peter", "Alex", "John", "Arthur", "Richard"];
+// const girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"];
+// const array3 = boys.concat(girls);
+
+// console.log(array3);
+// Expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+// const sort = arr.sort((a, b) => b - a);
+
+// console.log(arr);
