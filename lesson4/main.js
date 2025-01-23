9807eb26e5c44d231f8702a4493ba9acfe84ceca
Talgat-07 <talakv.07@gmail.com>
2025-01-23 19:53:02 +0600

    lesson 5

--- lesson4/main.js
+++ lesson4/main.js
@@ -1,51 +1,93 @@
 // const arr = [1, 2, 43, 5, 34, 5];
 
-//map
+// //map
+// "1253"[(1, 2, 5, 3)];
+// // const f = arr.findIndex((el) => el === 34);
 
-// const f = arr.findIndex((el) => el === 34);
+// // console.log(f);
 
-// console.log(f);
+// // arr.pop
+// // arr.shift
+// // arr.push
+// // arr.unshift
 
-// arr.pop
-// arr.shift
-// arr.push
-// arr.unshift
+// // console.log(arr);
+
+// // Метод pop() удаляет последний элемент из массива и возвращает его значение.
+// // const a = arr.pop();
+
+// // Метод shift() удаляет первый элемент из массива и возвращает его значение. Этот метод изменяет длину массива.
+// // const a = arr.shift();
+
+// // arr.push(100);
+// // arr.unshift(99);
+
+// // console.log(arr);
+
+// //filter
+
+// console.log(arr);
+
+// // const filter = arr.filter((el) => el > 10);
+
+// // const total = arr.reduce((acc, curr) => acc + curr, 0);
+
+// // const join = arr
+// //   .join(" ")
+// //   .split(" ")
+// //   .map((el) => Number(`${el}0`));
+
+// const splice = arr.splice(0, 2).join("");
+// console.log(splice);
+
+// // arr.splice(2, 0, 100);
+// // const boys = ["Peter", "Alex", "John", "Arthur", "Richard"];
+// // const girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"];
+// // const array3 = boys.concat(girls);
+
+// // console.log(array3);
+// // Expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+// // const sort = arr.sort((a, b) => b - a);
 
 // console.log(arr);
 
-// Метод pop() удаляет последний элемент из массива и возвращает его значение.
-// const a = arr.pop();
+function century(year) {
+  const str = year.toString();
 
-// Метод shift() удаляет первый элемент из массива и возвращает его значение. Этот метод изменяет длину массива.
-// const a = arr.shift();
+  if (str.length === 2) {
+    return 1;
+  }
 
-// arr.push(100);
-// arr.unshift(99);
+  if (str.length === 3) {
+    const num = str.split("").splice(0, 1).join("");
+    const numYear = Number(num + "00");
 
-// console.log(arr);
+    if (numYear < year) {
+      const totalNum = Number(num) + 1;
+      return totalNum;
+    } else {
+      return num;
+    }
+  }
 
-//filter
+  const num = str.split("").splice(0, 2).join("");
+  const numYear = Number(num + "00");
 
-// console.log(arr);
+  //num "19"
+  //numYear 1900
 
-// const filter = arr.filter((el) => el > 10);
+  if (numYear < year) {
+    const totalNum = Number(num) + 1;
+    return totalNum;
+  } else {
+    return num;
+  }
+}
 
-// const total = arr.reduce((acc, curr) => acc + curr, 0);
+Math.floor(1.1);
+Math.round(1.1);
 
-// const join = arr
-//   .join(" ")
-//   .split(" ")
-//   .map((el) => Number(`${el}0`));
-
-// arr.splice(3, 3);
-// arr.splice(2, 0, 100);
-// const boys = ["Peter", "Alex", "John", "Arthur", "Richard"];
-// const girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"];
-// const array3 = boys.concat(girls);
-
-// console.log(array3);
-// Expected output: Array ["a", "b", "c", "d", "e", "f"]
-
-// const sort = arr.sort((a, b) => b - a);
-
-// console.log(arr);
+console.log(Math.ceil(1.1));
+console.log(Math.floor(1.1));
+console.log(Math.round(1.1));
---
+++ lesson5/dom.js
@@ -0,0 +1,59 @@
+// console.log(document);
+
+// const h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+
+// const block = document.getElementsByClassName("block");
+// console.log(block[0]);
+
+// const text = document.getElementById("text");
+// console.log(text);
+
+// const rinat = document.querySelector("li");
+// console.log(rinat);
+
+// const arlen = document.querySelector(".list");
+// console.log(arlen);
+
+// const adilet = document.querySelector("#list");
+// console.log(adilet);
+
+// const arrayList = document.querySelectorAll("li");
+// console.log(arrayList);
+
+// const child = document.querySelector("#child");
+// console.log(child);
+
+// const parent = child.parentElement;
+// console.log(parent);
+
+// const twoItem = document.getElementById("twoItem");
+// console.log(twoItem);
+
+// console.log(twoItem.previousElementSibling);
+// console.log(twoItem.nextElementSibling);
+
+// const container = document.getElementById("container");
+
+// container.style.width = "300px";
+// container.style.height = "300px";
+// container.style.background = "green";
+
+// const h1 = document.createElement("h1");
+
+// h1.textContent = "Hello World!";
+// h1.style.color = "white";
+
+// container.appendChild(h1);
+
+// // h1.remove();
+
+// container.addEventListener("click", function () {
+//   h1.style.color = "red";
+// });
+
+// container.addEventListener("mouseleave", function () {
+//   h1.style.color = "white";
+// });
+
+// console.log(container);
